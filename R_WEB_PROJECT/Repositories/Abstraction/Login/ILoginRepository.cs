d7afaf2c109524e8f3a76a97f7be9499eaacf801
정종한 <howeer15325@naver.com>
2023-12-31 13:34:28 +0900

    IsAccountByIdPassAsync Model 리턴으로 변경 ADO.NET -> Dapper 사용으로 인한 변경

--- R_WEB_PROJECT/Repositories/Abstraction/Login/ILoginRepository.cs
+++ R_WEB_PROJECT/Repositories/Abstraction/Login/ILoginRepository.cs
@@ -1,10 +1,11 @@
 ﻿using R_WEB_PROJECT.Models.Login;
+using System.Data;
 
 namespace R_WEB_PROJECT.Repositories.Abstraction.Login
 {
 	public interface ILoginRepository
     {
         //아이디, 비밀번호로 계정 존재 여부 확인
-        Task<bool> IsAccountByIdPassAsync(AccountModel model);
+        Task<AccountModel> IsAccountByIdPassAsync(AccountModel model);
     }
 }
--- R_WEB_PROJECT/Repositories/Implementation/Login/LoginRepository.cs
+++ R_WEB_PROJECT/Repositories/Implementation/Login/LoginRepository.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using log4net.Repository.Hierarchy;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using R_WEB_PROJECT.Models.Login;
 using R_WEB_PROJECT.Repositories.Abstraction.Login;
 using R_WEB_PROJECT.Utilities.Database;
 using R_WEB_PROJECT.Utilities.Log;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace R_WEB_PROJECT.Repositories.Implementation.Login
 {
@@ -16,19 +22,16 @@ namespace R_WEB_PROJECT.Repositories.Implementation.Login
 		}
 
 		//아이디, 비밀번호로 계정 존재 여부 확인
-		public async Task<bool> IsAccountByIdPassAsync(AccountModel model)
+		public async Task<AccountModel> IsAccountByIdPassAsync(AccountModel model)
 		{
-			var query = "SELECT COUNT(*) FROM TB_ACCOUNT WHERE A_ID = @aId AND A_PASSWORD = @aPassword";
-			var parameters = new[]
-			{
-				new SqlParameter("@aId", model.aId),
-				new SqlParameter("@aPassword", model.aPassword)
-			};
+			var query = "SELECT A_NO, A_ID, A_PASSWORD, A_PASSWORD_SALT, A_NAME FROM TB_ACCOUNT WHERE A_ID = @aId AND A_PASSWORD = @aPassword";
 
-			Log.Debug("SQL", SqlPramMapper.MapQuery(query, parameters));
+			object parameters = new { aId = model.aId, aPassword = model.aPassword };
+			Log.Debug("SQL", SqlParamMapper.MapQuery(query, parameters));
 
-			var result = await _dbManager.GetSingleRecordAsync(query, parameters);
-			return (int)result > 0;
+			var result = await _dbManager.GetSingleRecordAsync<AccountModel>(query, parameters);
+
+			return result;
 		}
 	}
 }
