8320b6588f57030eeb092832e50624ae55cf745d
정종한 <howeer15325@naver.com>
2023-12-31 13:33:57 +0900

    ADO.NET -> Dapper 사용으로 인한 변경

--- R_WEB_PROJECT/Services/Abstraction/Login/ILoginService.cs
+++ R_WEB_PROJECT/Services/Abstraction/Login/ILoginService.cs
@@ -1,4 +1,5 @@
 ﻿using R_WEB_PROJECT.Models.Login;
+using System.Data;
 
 namespace R_WEB_PROJECT.Services.Abstraction.Login
 {
@@ -6,6 +7,5 @@ namespace R_WEB_PROJECT.Services.Abstraction.Login
 	{
 		//아이디, 비밀번호로 계정 존재 여부 확인 서비스
 		Task<bool> IsAccountByIdPassAsync(AccountModel model);
-
 	}
 }
--- R_WEB_PROJECT/Services/Implementation/Login/LoginService.cs
+++ R_WEB_PROJECT/Services/Implementation/Login/LoginService.cs
@@ -1,12 +1,14 @@
 ﻿using R_WEB_PROJECT.Models.Login;
 using R_WEB_PROJECT.Repositories.Abstraction.Login;
 using R_WEB_PROJECT.Services.Abstraction.Login;
+using R_WEB_PROJECT.Utilities.Log;
 
 namespace R_WEB_PROJECT.Services.Implementation.Login
 {
-	public class LoginService : ILoginService
+    public class LoginService : ILoginService
 	{
 		private readonly ILoginRepository _loginRepository;
+
 
 		public LoginService(ILoginRepository loginRepository)
 		{
@@ -16,7 +18,19 @@ namespace R_WEB_PROJECT.Services.Implementation.Login
 		//아이디, 비밀번호로 계정 존재 여부 확인 서비스
 		public async Task<bool> IsAccountByIdPassAsync(AccountModel model)
 		{
-			return await _loginRepository.IsAccountByIdPassAsync(model);
+			bool isPass = false;
+
+			var accountInfo = await _loginRepository.IsAccountByIdPassAsync(model);
+			if (accountInfo != null)
+			{
+				isPass = true;
+				Log.Debug("SYSTEM", $"Retrieved aId = {accountInfo.aId}");
+				Log.Debug("SYSTEM", $"Retrieved aPassword = {accountInfo.aPassword}");
+				Log.Debug("SYSTEM", $"Retrieved aName = {accountInfo.aName}");
+				Log.Debug("SYSTEM", $"Retrieved aPasswordSalt = {accountInfo.aPasswordSalt}");
+			}
+
+			return isPass;
 		}
 	}
 }
