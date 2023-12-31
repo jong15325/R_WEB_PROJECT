63870568c77667b5cda2f866cf2164ce19197814
정종한 <howeer15325@naver.com>
2023-12-31 13:32:34 +0900

    ADO.NET -> Dapper 사용 변경

--- R_WEB_PROJECT/Utilities/Database/DatabaseManager.cs
+++ R_WEB_PROJECT/Utilities/Database/DatabaseManager.cs
@@ -1,150 +1,60 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 public class DatabaseManager : IDisposable
 {
 	private readonly string _connectionString;
-	private readonly SqlConnection _connection;
 
 	public DatabaseManager(string connectionString)
 	{
 		_connectionString = connectionString;
-		_connection = new SqlConnection(_connectionString);
 	}
 
-	// 커넥션 OPEN
-	private async Task OpenConnectionAsync()
+	public async Task<IDbConnection> GetOpenConnectionAsync()
 	{
-		if (_connection.State != ConnectionState.Open)
-			await _connection.OpenAsync();
-	}
-
-	//커넥션 GET
-	public async Task<SqlConnection> GetConnectionAsync()
-	{
-		await OpenConnectionAsync();
-		return _connection;
+		var connection = new SqlConnection(_connectionString);
+		await connection.OpenAsync();
+		return connection;
 	}
 
 	//단일 레코드 조회
-	public async Task<object> GetSingleRecordAsync(string query, SqlParameter[] parameters = null)
+	public async Task<T> GetSingleRecordAsync<T>(string query, object parameters)
 	{
-		using (var connection = await GetConnectionAsync()) {
-			using (var command = new SqlCommand(query, connection))
-			{
-				if (parameters != null)
-					command.Parameters.AddRange(parameters);
-
-				return await command.ExecuteScalarAsync();
-			}
+		using (var connection = await GetOpenConnectionAsync())
+		{
+			return await connection.QueryFirstOrDefaultAsync<T>(query, parameters);
 		}
 	}
 
 	//다중 레코드 조회
-	public async Task<DataTable> GetMultiRecordAsync(string query, SqlParameter[] parameters = null)
+	public async Task<IEnumerable<T>> GetMultiRecordsAsync<T>(string query, object parameters)
 	{
-		using (var connection = await GetConnectionAsync()) {
-			using (var command = new SqlCommand(query, connection))
-			{
-				if (parameters != null)
-					command.Parameters.AddRange(parameters);
-
-				var dataTable = new DataTable();
-				var reader = await command.ExecuteReaderAsync();
-
-				dataTable.Load(reader);
-				return dataTable;
-			}
-		}
-	}
-
-	//쿼리 실행 및 성공 행 수 반환
-	public async Task<int> ExecuteNonQueryAsync(string query, SqlParameter[] parameters = null)
-	{
-		using (var connection = await GetConnectionAsync()) {
-			using (var command = new SqlCommand(query, connection))
-			{
-				if (parameters != null)
-					command.Parameters.AddRange(parameters);
-
-				return await command.ExecuteNonQueryAsync();
-			}
-		}
-	}
-
-	//한꺼번에 트랙잭션 처리
-	public async Task ExecuteTransactionAsync(string[] queries, SqlParameter[][] parameters = null, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
-	{
-		using (var connection = await GetConnectionAsync())
+		using (var connection = await GetOpenConnectionAsync())
 		{
-			var transaction = connection.BeginTransaction(isolationLevel);
-
-			try
-			{
-				foreach (var query in queries)
-				{
-					using (var command = new SqlCommand(query, connection, transaction))
-					{
-						if (parameters != null && parameters.Length > 0)
-						{
-							command.Parameters.AddRange(parameters[Array.IndexOf(queries, query)]);
-						}
-
-						await command.ExecuteNonQueryAsync();
-					}
-				}
-
-				transaction.Commit();
-			}
-			catch (Exception)
-			{
-				transaction.Rollback();
-				throw;
-			}
+			return await connection.QueryAsync<T>(query, parameters);
 		}
 	}
 
-	//벌크 인서트
-	public async Task BulkInsertAsync(DataTable dataTable, string tableName)
+	//NSERT, UPDATE, DELETE 실행 후 행 갯수 반환
+	public async Task<int> ExecuteNonQueryAsync(string query, object parameters)
 	{
-		using (var connection = new SqlConnection(_connectionString))
+		using (var connection = await GetOpenConnectionAsync())
 		{
-			await connection.OpenAsync();
-
-			using (var bulkCopy = new SqlBulkCopy(connection))
-			{
-				bulkCopy.DestinationTableName = tableName;
-
-				foreach (DataColumn column in dataTable.Columns)
-				{
-					bulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
-				}
-
-				await bulkCopy.WriteToServerAsync(dataTable);
-			}
+			return await connection.ExecuteAsync(query, parameters);
 		}
 	}
 
-	//인서트 및 IDX 반환
-	public async Task<int> InsertAndReturnIdAsync(string query, SqlParameter[] parameters = null)
+	public async Task<int> InsertAndReturnIdAsync(string query, object parameters)
 	{
-		using (var connection = await GetConnectionAsync())
+		using (var connection = await GetOpenConnectionAsync())
 		{
-			query += ";SELECT SCOPE_IDENTITY();"; // 이 부분은 해당 테이블의 자동 증가하는 ID 값을 반환하는 SQL 문을 사용하는 예시입니다.
-
-			using (var command = new SqlCommand(query, connection))
-			{
-				if (parameters != null)
-					command.Parameters.AddRange(parameters);
-
-				var result = await command.ExecuteScalarAsync();
-				return Convert.ToInt32(result);
-			}
+			query += ";SELECT SCOPE_IDENTITY();";
+			return await connection.ExecuteScalarAsync<int>(query, parameters);
 		}
 	}
 
 	public void Dispose()
 	{
-		_connection?.Dispose();
 	}
 }
