c74a6ed5ce38f0470c29d3c6ac0c3ea19882d9a0
정종한 <howeer15325@naver.com>
2023-12-31 13:32:09 +0900

    클래스 이름 변경 및 기존 SqlParameter[] 인자 -> object 인자로 변경(Dapper 사용으로 인한)

---
+++ R_WEB_PROJECT/Utilities/Database/SqlParamMapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace R_WEB_PROJECT.Utilities.Database
+{
+	public static class SqlParamMapper
+	{
+		public static string MapQuery(string query, object parameters)
+		{
+			if (parameters != null)
+			{
+				var properties = parameters.GetType().GetProperties();
+
+				foreach (var prop in properties)
+				{
+					// 파라미터 이름 가져오기
+					var paramName = prop.Name;
+					var paramValue = prop.GetValue(parameters);
+
+					// 정규식을 사용하여 쿼리와 일치하는 파라미터를 찾아 매핑
+					var regex = new Regex($@"@{paramName}\b");
+					var match = regex.Match(query);
+
+					if (match.Success)
+					{
+						query = query.Replace(match.Value, "'" + paramValue?.ToString() + "'");
+					}
+				}
+			}
+				
+
+			return query;
+		}
+	}
+}
