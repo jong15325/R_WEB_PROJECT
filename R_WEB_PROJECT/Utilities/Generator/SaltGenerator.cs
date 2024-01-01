cb499ce6ad0e4629145ee6b488f2583e5e71ceea
정종한 <howeer15325@naver.com>
2024-01-01 20:52:26 +0900

    패스워드 검증 -> SHA256 + salt 사용
    salt 생성 및 비밀번호 검증 클래스 추가

---
+++ R_WEB_PROJECT/Utilities/Generator/SaltGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace R_WEB_PROJECT.Utilities.Generator
+{
+	public class SaltGenerator
+	{
+		public static string GenerateRandomSalt(int size)
+		{
+			var rngCrypto = new RNGCryptoServiceProvider();
+			var buff = new byte[size];
+			rngCrypto.GetBytes(buff);
+			return Convert.ToBase64String(buff);
+		}
+
+	}
+}
