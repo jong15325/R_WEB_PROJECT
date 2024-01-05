e0eb484a9a6ebd35a29805c9be6765e1f8ef295a
정종한 <howeer15325@naver.com>
2024-01-05 15:28:38 +0900

    패스워드 해시 수정 및 클래스 이름 변경

---
+++ R_WEB_PROJECT/Utilities/password/PasswordManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Identity;
+using R_WEB_PROJECT.Utilities.Enums;
+using R_WEB_PROJECT.Utilities.Generator;
+using R_WEB_PROJECT.Utilities.Log;
+using System.Text;
+
+namespace R_WEB_PROJECT.Utilities.password
+{
+	public class PasswordManager
+	{
+		public static string HashPassword(string inputPassword, string UserStoreSalt, bool isCreate)
+		{
+			Log.Log.Debug("SECUTIRY", $"inputPassword = {inputPassword} / UserStoreSalt = {UserStoreSalt} / isCreate = {isCreate}");
+
+			string salt = UserStoreSalt;
+
+			if (isCreate) 
+			{
+				salt = SaltGenerator.GenerateRandomSalt(32);
+				Log.Log.Debug("SECUTIRY", $"New Salt = {salt}");
+			}
+
+			using (var sha256 = System.Security.Cryptography.SHA256.Create())
+			{
+				byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(inputPassword + salt));
+				string hash = Convert.ToBase64String(hashedBytes);
+				return hash;
+			}
+		}
+
+		public static bool VerifyPassword(string hashedPassword, string storedPassword)
+		{
+			return hashedPassword == storedPassword;
+		}
+	}
+}
