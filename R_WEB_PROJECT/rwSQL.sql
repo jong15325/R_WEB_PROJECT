2a27a8c485ebc2110dc25f9473a136b78a74c619
정종한 <howeer15325@naver.com>
2024-01-10 17:40:02 +0900

    db 스키마

---
+++ R_WEB_PROJECT/rwSQL.sql
@@ -0,0 +1,18 @@
+-- RWEB.dbo.Account definition
+
+-- Drop table
+
+-- DROP TABLE RWEB.dbo.Account;
+
+CREATE TABLE RWEB.dbo.Account (
+	idx int IDENTITY(0,1) NOT NULL,
+	UserId nvarchar(50) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	UserType nvarchar(10) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	UserPassword nvarchar(255) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	UserPasswordSalt nvarchar(255) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	UserName nvarchar(30) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	UserRoleCd nvarchar(20) COLLATE Korean_Wansung_CI_AS NULL,
+	UserCreateAt datetime DEFAULT getdate() NOT NULL,
+	UserUpdateAt datetime DEFAULT getdate() NOT NULL,
+	UserDeleteAt datetime
+);
