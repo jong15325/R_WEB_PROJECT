9759d5b8273d3655e239063f6b82914a368f918a
정종한 <howeer15325@naver.com>
2024-01-16 10:00:11 +0900

    알럿 리소스 추가

---
+++ R_WEB_PROJECT/Utilities/Enums/AlertEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace R_WEB_PROJECT.Utilities.Enums
+{
+    public class AlertEnum
+    {
+        public enum AlertType
+        {
+            [Description("Error")]
+            Error,
+
+            [Description("Scuccess")]
+            Scuccess,
+
+            [Description("Warning")]
+            Warning,
+
+            [Description("Info")]
+            Info,
+        }
+    }
+}
