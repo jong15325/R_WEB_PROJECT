f9de25efa84c1f068616c392cd659f99263d6eb0
정종한 <howeer15325@naver.com>
2024-01-16 17:14:49 +0900

    로그 삭제

---
+++ R_WEB_PROJECT/Utilities/Manager/MessageManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Localization;
+using R_WEB_PROJECT.Resources;
+
+namespace R_WEB_PROJECT.Utilities.Manager
+{
+    public class MessageManager
+    {
+
+        private readonly IStringLocalizer<SharedResource> _localizer;
+
+        public MessageManager(IStringLocalizer<SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public string GetMessage(string messageKey)
+        {
+            return _localizer[messageKey];
+        }
+    }
+}
