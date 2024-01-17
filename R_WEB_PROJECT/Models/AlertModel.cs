859885cbb524350edc09f1b2f772f2627d8fa4b6
정종한 <howeer15325@naver.com>
2024-01-17 16:47:36 +0900

    알럿 모델 추가

---
+++ R_WEB_PROJECT/Models/AlertModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace R_WEB_PROJECT.Models
+{
+    public class AlertModel
+    {
+        public AlertModel()
+        {
+            Title = string.Empty;
+            Message = string.Empty;
+            AlertType = string.Empty;
+            AlertIconType = string.Empty;
+        }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public string AlertType { get; set; }
+
+        public string AlertIconType { get; set; }
+
+        //TempData는 String반환이라 json으로 변환해줘야함
+        public string ToJsonString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static AlertModel FromJsonString(string jsonString)
+        {
+            return JsonSerializer.Deserialize<AlertModel>(jsonString);
+        }
+    }
+}
