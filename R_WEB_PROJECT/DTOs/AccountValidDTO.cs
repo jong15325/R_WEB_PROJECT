e22506c4a75bc0bd3afb5e6bcf7b71c25f94ab46
정종한 <howeer15325@naver.com>
2024-01-17 16:45:23 +0900

    파일 구성 변경

---
+++ R_WEB_PROJECT/DTOs/AccountValidDTO.cs
@@ -0,0 +1,11 @@
+﻿using R_WEB_PROJECT.Models;
+
+namespace R_WEB_PROJECT.DTOs
+{
+    public class AccountValidDTO
+    {
+        public bool IsPass { get; set; }
+        public AccountModel AccountInfo { get; set; }
+        public string Result { get; set; }
+    }
+}
