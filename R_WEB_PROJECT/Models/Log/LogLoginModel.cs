081f9a7543ee3be17017bb3bd7e9d632a8bb7bfb
정종한 <howeer15325@naver.com>
2024-01-18 12:28:25 +0900

    로그_로그인 모델 추가

---
+++ R_WEB_PROJECT/Models/Log/LogLoginModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace R_WEB_PROJECT.Models.Log
+{
+	public class LogLoginModel
+	{
+		public LogLoginModel()
+		{
+			Idx = 0;
+			LoginUserId = string.Empty;
+			LoginAt = DateTime.Now;
+			LoginIp = string.Empty;
+			LoginAgent = string.Empty;
+			LoginStatus = string.Empty;
+		}
+
+		[Key]
+		[Required]
+		public int Idx { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string LoginUserId { get; set; }
+
+		[Required]
+		public DateTime LoginAt { get; set; }
+
+		[Required]
+		[StringLength(45)]
+		public string LoginIp { get; set; }
+
+		[Required]
+		[MaxLength]
+		public string LoginAgent { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		public string LoginStatus { get; set; }
+
+		public override string ToString()
+		{
+			return $"Idx[{Idx}], LoginUserId[{LoginUserId}], LoginAt[{LoginAt}], LoginIp[{LoginIp}], LoginAgent[{LoginAgent}]," +
+				$"LoginStatus[{LoginStatus}]";
+		}
+	}
+}
