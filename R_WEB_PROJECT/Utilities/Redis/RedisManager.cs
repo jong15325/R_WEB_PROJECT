583dd271387505eec87b1163084c14fec4a667ca
정종한 <howeer15325@naver.com>
2024-01-18 12:11:47 +0900

    레디스 스토어 이름 변경

---
+++ R_WEB_PROJECT/Utilities/Redis/RedisManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace R_WEB_PROJECT.Utilities.Redis
+{
+    public class RedisManager
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public RedisManager(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        //세션 셋팅
+        public async Task SetRedisAsync<T>(string key, T value, TimeSpan expirationTime)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = expirationTime
+            };
+
+            string serializedValue = JsonSerializer.Serialize(value);
+            await _distributedCache.SetStringAsync(key, serializedValue, options);
+            Log.Log.Debug("REDIS", string.Format("SET key :{0} / serializedValue = {{{1}}}", key, serializedValue));
+        }
+
+        //세션 불러오기
+        public async Task<T> GetRedisAsync<T>(string key)
+        {
+            var serializedValue = await _distributedCache.GetStringAsync(key);
+            if (serializedValue == null)
+                return default;
+
+            Log.Log.Debug("REDIS", string.Format("GET key :{0} / serializedValue = {{{1}}}", key, serializedValue));
+
+            var returnValue = JsonSerializer.Deserialize<T>(serializedValue);
+
+            return returnValue;
+        }
+
+        //세션 삭제
+        public async Task RemoveRedisAsync(string key)
+        {
+            await _distributedCache.RemoveAsync(key);
+        }
+    }
+}
