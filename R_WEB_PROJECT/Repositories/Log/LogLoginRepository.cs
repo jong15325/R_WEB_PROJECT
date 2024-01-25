c7e1e1fdaad70154dd21646e169fdf8ccebc98a6
정종한 <howeer15325@naver.com>
2024-01-25 13:51:51 +0900

    불필요 파일 삭제 및 서비스, 레포지토리 경로 단순화 및 변경

---
+++ R_WEB_PROJECT/Repositories/Log/LogLoginRepository.cs
@@ -0,0 +1,52 @@
+﻿using R_WEB_PROJECT.Models.Log;
+using R_WEB_PROJECT.Models.User;
+using R_WEB_PROJECT.Repositories.Abstraction.Login;
+using R_WEB_PROJECT.Utilities.Database;
+using R_WEB_PROJECT.Utilities.Log;
+
+namespace R_WEB_PROJECT.Repositories.Log
+{
+    public interface ILogLoginRepository
+    {
+        //아이디, 비밀번호로 계정 존재 여부 확인
+        Task<int> InsertLogLoginAsync(LogLoginModel model);
+    }
+
+    public class LogLoginRepository : ILogLoginRepository
+    {
+        private readonly DatabaseManager _dbManager;
+
+        public LogLoginRepository(DatabaseManager dbManager)
+        {
+            _dbManager = dbManager;
+        }
+
+        //아이디, 비밀번호로 계정 존재 여부 확인
+        public async Task<int> InsertLogLoginAsync(LogLoginModel model)
+        {
+            const string query = "SELECT * FROM Account WHERE UserId = @UserId";
+
+            object parameters = new
+            {
+                model.LoginUserId,
+                model.LoginIp,
+                model.LoginAgent,
+                model.LoginStatus
+            };
+
+            LogUtil.Debug("SQL", SqlParamMapper.MapQuery(query, parameters));
+
+            try
+            {
+                int result = await _dbManager.ExecuteNonQueryAsync(query, parameters);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogUtil.Error("SYSTEM", $"An error occurred during InsertLogLoginAsync Repository : {ex.GetType().Name} - {ex.Message}", ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ R_WEB_PROJECT/Services/Log/LogLoginService.cs
@@ -0,0 +1,47 @@
+﻿using R_WEB_PROJECT.DTOs;
+using R_WEB_PROJECT.Models.Log;
+using R_WEB_PROJECT.Repositories.Abstraction.Login;
+using R_WEB_PROJECT.Services.Abstraction.Log;
+using R_WEB_PROJECT.Utilities.Log;
+using R_WEB_PROJECT.Utilities.password;
+using static R_WEB_PROJECT.Utilities.Enums.LoginEnum;
+
+namespace R_WEB_PROJECT.Services.Log
+{
+    public interface ILogLoginService
+    {
+        //아이디, 비밀번호로 계정 존재 여부 확인 서비스
+        Task<int> InsertLogLoginAsync(LogLoginModel model);
+    }
+
+    public class LogLoginService : ILogLoginService
+    {
+        private readonly ILogLoginRepository _logLoginRepository;
+
+        public LogLoginService(ILogLoginRepository logLoginRepository)
+        {
+            _logLoginRepository = logLoginRepository;
+        }
+
+        public async Task<int> InsertLogLoginAsync(LogLoginModel model)
+        {
+            try
+            {
+                LogUtil.Debug("SYSTEM", "=============================== InsertLogLoginAsync Start ===============================");
+
+                string status = GetResultMessage(LoginResult.Success);
+
+                int result = await _logLoginRepository.InsertLogLoginAsync(model);
+
+                LogUtil.Debug("SYSTEM", "=============================== InsertLogLoginAsync End ===============================");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogUtil.Error("SYSTEM", $"An error occurred during InsertLogLoginAsync Service : {ex.GetType().Name} - {ex.Message}", ex);
+                throw;
+            }
+        }
+    }
+}
