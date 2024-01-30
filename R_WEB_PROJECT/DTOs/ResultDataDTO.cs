1ce368198d2af2811fa258d5a33762d248896375
정종한 <howeer15325@naver.com>
2024-01-30 17:26:56 +0900

    알럿, 메세지 등 반환 값에 대한 객체화

---
+++ R_WEB_PROJECT/DTOs/ResultDataDTO.cs
@@ -0,0 +1,23 @@
+﻿using static R_WEB_PROJECT.Utilities.Enums.AlertEnum;
+
+namespace R_WEB_PROJECT.DTOs
+{
+    public class ResultDataDTO
+    {
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        public AlertIconType IconType { get; set; }
+
+        public bool Status { get; set; }
+
+        public ResultDataDTO(string message, object data, AlertIconType iconType, bool status)
+        {
+            Message = message;
+            Data = data;
+            IconType = iconType;
+            Status = status;
+        }
+    }
+}
---
+++ R_WEB_PROJECT/Utilities/Data/ResultDataUtil.cs
@@ -0,0 +1,20 @@
+﻿using static R_WEB_PROJECT.Utilities.Enums.AlertEnum;
+
+namespace R_WEB_PROJECT.Utilities.Data
+{
+    public class ResultDataUtil
+    {
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        public AlertIconType IconType { get; set; }
+
+        public ResultDataUtil(string message, object data, AlertIconType iconType)
+        {
+            Message = message;
+            Data = data;
+            IconType = iconType;
+        }
+    }
+}
