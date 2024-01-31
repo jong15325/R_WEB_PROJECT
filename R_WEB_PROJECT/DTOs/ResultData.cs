2d21b5a0fdc540c698dc9f8bbeeff0b842982ea5
정종한 <howeer15325@naver.com>
2024-01-31 17:26:40 +0900

    알럿 타이틀 추가

--- R_WEB_PROJECT/DTOs/ResultData.cs
+++ R_WEB_PROJECT/DTOs/ResultData.cs
@@ -8,14 +8,17 @@ namespace R_WEB_PROJECT.DTOs
 
         public AlertEnum.AlertIconType  AlertIconType { get; set; } 
 
+        public string Title { get; set; }
+
         public string Message { get; set; }
 
         public int StatusCode { get; set; }
 
-        public ResultData(AlertEnum.AlertType alertType, AlertEnum.AlertIconType alertIconType, string message, int statusCode)
+        public ResultData(AlertEnum.AlertType alertType, AlertEnum.AlertIconType alertIconType, string title, string message, int statusCode)
         {
             AlertType = alertType;
             AlertIconType = alertIconType;
+            Title = title;
             Message = message;
             StatusCode = statusCode;
         }
